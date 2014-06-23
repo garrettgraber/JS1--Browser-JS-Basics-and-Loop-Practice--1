4ba0d6bb60fecca12e5073a767a9a75f4ffb5a14
garrettgraber <garrett_graber@yahoo.com>
2014-06-23 11:23:59 -0600

    JS1 and loop practice done

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+
+
+console.log('Hello Boulder!');
+
+var input_info = prompt("Name", "");
+
+var total_string = "Hello " + input_info +"!";
+
+alert(total_string);
+
+
+
+for(i = 0; i < 6; i++) {
+	output_val = i * 100;
+	console.log(output_val);
+}
+
+for(i = 0; i < 7; i++) {
+	output_val = Math.pow(2,i);
+	console.log(output_val);
+}
+
+for(i = 1; i < 4; i++) {
+	for(j = 1; j < 4; j++) {
+		console.log(i);
+	}
+}
+
+for(i = 0; i < 6; i++) {
+	output_val = i * 2;
+	console.log(output_val);
+}
+
+for(i = 1; i < 6; i++) {
+	output_val = i * 3;
+	console.log(output_val);
+}
+
+for(i = 9; i >= 0; i--) {
+	console.log(i);
+}
+
+for(i = 1; i < 4; i++) {
+	for(j = 0; j < 4; j++) {
+		console.log(j);
+	}
+}
+
